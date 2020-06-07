fd2fbbefb18e3eb4447558754bafeb85388db158
Josh Blumberg <jlblumberg@gmail.com>
2020-06-07 17:04:33 +0100

    Update act warning

--- src/setupTests.js
+++ src/setupTests.js
@@ -10,3 +10,20 @@ jest.mock('react-chartjs-2', () => ({
 jest.mock('react-chartjs-2', () => ({
   Bar: () => null
 }));
+
+const mockConsoleMethod = (realConsoleMethod) => {
+  const ignoredMessages = [
+    'test was not wrapped in act(...)',
+  ]
+
+  return (message, ...args) => {
+    const containsIgnoredMessage = ignoredMessages.some(ignoredMessage => message.includes(ignoredMessage))
+
+    if (!containsIgnoredMessage) {
+      realConsoleMethod(message, ...args)
+    }
+  }
+}
+
+console.warn = jest.fn(mockConsoleMethod(console.warn))
+console.error = jest.fn(mockConsoleMethod(console.error))
